faf7ec56a622c0a9ed9f143497b31f8d49e1cab1
Sang Kim <sangkim1@uw.edu>
2015-11-09 09:46:23 -0800

    monday class

--- js/app.js
+++ js/app.js
@@ -1,13 +1,44 @@
 // Create app
+var myApp = angular.module('myApp', ['ui.router'])
 
 // Configure app
+myApp.config(function($stateProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'templates/landing.html',
+            controller: 'HomeController'
+        })
+        .state('content', {
+            url: '/content',
+            templateUrl: 'templates/content.html',
+            controller: 'ContentController'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: 'templates/about.html',
+            controller: 'AboutController'
+        })
+})
 
 // Landing page controller: define $scope.number as a number
+myApp.controller('HomeController', function($scope) {
+    $scope.number = 6
+})
 
+myApp.controller('AboutController', function($scope) {
+    $scope.about = "Some info"
+})
 
+myApp.controller('ContentController', function($scope) {
+    $scope.url = "http://www.tattoosforyou.org/wp-content/uploads/2013/09/Mystic-Owl-Tattoo.jpg"
+})
 // About page controller: define $scope.about as a string
 
 
 // Content controller: define $scope.url as an image
+
+
+
 
 
